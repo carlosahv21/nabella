9693a83d14b749882ce59786104e7af21abfa9af
carlos hernandez <carloshernandez@MacBook-Air-de-carlos.local>
2025-01-29 16:10:33 -0400

    Add favicon

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -20,7 +20,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets') }}/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="{{ asset('assets') }}/img/favicon.png">
+    <link rel="icon" type="image/png" href="{{ asset('assets') }}/img/favicon.ico">
     <title>
         Nabella Transportation LLC
     </title>
